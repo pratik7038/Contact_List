6b502bef0a21696359e3e2c1ac955c47dd00b637
Pratik Patharkar <pratik.21810592@viit.ac.in>
2022-03-28 11:21:24 +0530

    DB introduced

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const path = require("path");
 const express= require("express");
 const { redirect } = require("express/lib/response");
 
+const db = require('./config/mongoose')
+
+const Contact = require('./models/contact')
+ 
+
 const app = express();
 //body parser
 const bodyParser = require('body-parser');
+const req = require("express/lib/request");
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('view engine','ejs');
@@ -15,26 +21,36 @@ app.set('views',path.join(__dirname,'views'));
 
 app.use(express.static("assets"));
 
-var contactList = [
-    {   
-        name:"Pratik",
-        phone_no:"70283929390"
-    },
-    {
-        name:"diksha",
-        phone_no:"983929390"
-    }
-]
+// code for mongodb atlas // cloud based approach
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://pratik:pratik@cluster0.nwome.mongodb.net/CONTACT_LIST?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//     console.log(collection)
+//   client.close();
+// });
 
+
+ 
 app.get('/',function(req,res){
     // fs.readFile(filepath,function(err,data){
     //     res.end(data);
     // // });
     // console.log(__dirname);
     // res.send("<h1>Hello</h1>") 
-    res.render('home',{
+
+
+    Contact.find({},function(err,ContactListFromDb ){  
+        
+        if(err)console.log(err);
+        else
+
+        return res.render('home',{
         title : "Contact List",
-        contact_list : contactList
+        contact_list :  ContactListFromDb
+        });
+    
     });
 });
 app.get('/submit',function(req,res){
@@ -42,19 +58,21 @@ app.get('/submit',function(req,res){
 })
 
 app.post('/create-contact',function(req,res){   
-    contactList.push({
+
+    Contact.create({
         name:req.body.myname,
         phone_no:req.body.myphone
+    },(err,newContact)=>{
+        if(err)console.log(err);
+        else console.log(newContact);
     });
-    console.log(contactList);
-    return res.redirect('/' );
+ 
+    return res.redirect('back' );
 })
 
 
 app.get('/delete-contact',function(req,res){ 
-    let phonenum = req.query.phone;  
-    var temp;
-    console.log("abcd")
+    let phonenum = req.query.phone;   
     console.log(req.query)
 
     for( var i = 0; i < contactList.length; i++){ 
